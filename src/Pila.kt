69905a2acfc933af431b795bd5677761d4784cb7
rmungar1209 <rmungar1209@g.educaand.es>
2024-02-28 17:02:05 +0100

    Terminado y comentado

--- src/Pila.kt
+++ src/Pila.kt
@@ -1,18 +1,40 @@
-
+/**
+ * Clase genérica que va a actuar como un stack con sus métodos característicos
+ * @property contenido -> Contenido del stack
+ */
 class Pila <T>(){
     private val contenido = mutableListOf<T>()
 
+    /**
+     * Retorna el elemento superior del stack
+     * @return T
+     */
     fun top(): T {
         return contenido[contenido.size-1]
     }
 
+    /**
+     * Introduce un nuevo elemento en el stack
+     * @param item -> Elemento a introducir en el stack
+     */
     fun push(item: T){
         contenido.add(item)
     }
+
+    /**
+     * Retorna el elemento superior del stack
+     * @return Null si no hay elemento
+     * @return Dicho elemento
+     */
     fun pop(): T?{
         return if (contenido.isEmpty()) null
         else contenido.removeAt(contenido.size - 1)
     }
+
+    /**
+     * Comprueba el contenido del stack y retorna true si contiene algo o false si esta vacío
+     * @return Boolean
+     */
     fun isEmpty(): Boolean{
         return contenido.isEmpty()
     }
--- src/Usuario.kt
+++ src/Usuario.kt
@@ -1,2 +1,7 @@
+/**
+ * Data Class que contiene información osbre un usuario
+ * @property nombre -> String con el nombre del usuario
+ * @property edad -> Entero que representa la edad del usuario
+ */
 data class Usuario(val nombre: String, var edad: Int) {
 }
